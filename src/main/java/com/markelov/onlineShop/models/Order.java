b53096b7e55261bb6d362156a11821a2d145382a
Alex Markelov <AleMar-91@mail.ru>
2020-07-24 11:26:38 +0300

    second commit

--- src/main/java/com/markelov/onlineShop/models/Order.java
+++ src/main/java/com/markelov/onlineShop/models/Order.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table (name="ordr")
 public class Order {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue (strategy=GenerationType.IDENTITY)
 	private Long id;
 	
 	private String title;
